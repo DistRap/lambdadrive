f3c150a6863da09f12ccd045358cfa1509175b87
Richard Marko <srk@48.io>
2017-05-04 22:45:30 +0200

    DRV: make standalone, add ready and fault handling

--- src/ODrive/DRV8301/Regs.hs
+++ src/ODrive/DRV8301/Regs.hs
@@ -10,8 +10,13 @@ module ODrive.DRV8301.Regs where
 
 import Ivory.Language
 import ODrive.DRV8301.RegTypes
+import ODrive.Ivory.Types.DrvFault
 
 [ivory|
+ bitdata Drv8301 :: Bits 16 = drv_dat_reg
+   { drv_rw :: DrvRW
+   , drv_addr :: DrvAddr
+   , drv_data :: Bits 11 }
 
  bitdata CTRL1 :: Bits 11 = ctrl1
   { drv_ocadj :: OCAdj
@@ -29,4 +34,47 @@ import ODrive.DRV8301.RegTypes
    , drv_gain :: Gain
    , drv_octw :: OCTWMode
    }
+
+  bitdata STATUS1 :: Bits 11 = status1
+   { drv_sta1_fault    :: Bit
+   , drv_sta1_gvdd_uv  :: Bit
+   , drv_sta1_pvdd_uv  :: Bit
+   , drv_sta1_otsd     :: Bit -- overtemperature shutdown
+   , drv_sta1_otw      :: Bit -- overtemperature warning
+   , drv_sta1_fetha_oc :: Bit
+   , drv_sta1_fetla_oc :: Bit
+   , drv_sta1_fethb_oc :: Bit
+   , drv_sta1_fetlb_oc :: Bit
+   , drv_sta1_fethc_oc :: Bit
+   , drv_sta1_fetlc_oc :: Bit
+   }
+
+  bitdata STATUS2 :: Bits 11 = status2
+   { _                :: Bits 3
+   , drv_sta2_gvdd_ov :: Bit
+   , _                :: Bits 3
+   , drv_sta2_dev_id  :: Bits 4
+   }
 |]
+
+
+drvFaultFromRegs :: Uint16 -> Uint16 -> Ref s ('Struct "drv_fault") -> Ivory eff ()
+drvFaultFromRegs s1 s2 r = do
+  p1 fault              drv_sta1_fault
+  p1 gvdd_undervoltage  drv_sta1_gvdd_uv
+  p2 gvdd_overvoltage   drv_sta2_gvdd_ov
+  p1 pvdd_undervoltage  drv_sta1_pvdd_uv
+  p1 overtemp_shutdown  drv_sta1_otsd
+  p1 overtemp_warning   drv_sta1_otw
+
+  p1 fet_hi_a_overcurr  drv_sta1_fetha_oc
+  p1 fet_lo_a_overcurr  drv_sta1_fetla_oc
+
+  p1 fet_hi_b_overcurr  drv_sta1_fethb_oc
+  p1 fet_lo_b_overcurr  drv_sta1_fetlb_oc
+
+  p1 fet_hi_c_overcurr  drv_sta1_fethc_oc
+  p1 fet_lo_c_overcurr  drv_sta1_fetlc_oc
+  where
+    p1 lbl field = store (r ~> lbl) (bitToBool (fromRep s1 #. field))
+    p2 lbl field = store (r ~> lbl) (bitToBool (fromRep s2 #. field))
--- src/ODrive/Tests/SPI.hs
+++ src/ODrive/Tests/SPI.hs
@@ -16,166 +16,15 @@ import Ivory.Tower.HAL.Bus.Interface
 import Ivory.Tower.HAL.Bus.Sched
 
 import Ivory.BSP.STM32.ClockConfig
-import Ivory.BSP.STM32.Driver.UART
 import Ivory.BSP.STM32.Driver.SPI
-import Ivory.BSP.STM32.Peripheral.SPI -- as SPI
+import Ivory.BSP.STM32.Driver.UART
+import Ivory.BSP.STM32.Peripheral.SPI
 
 import ODrive.Platforms
 import ODrive.LED
 import ODrive.DRV8301
 import ODrive.Types
 import ODrive.Utils
-
-[ivory|
-  bitdata MSG :: Bits 16 = drv8301msg
-   { d_rw :: DrvRW
-   , d_addr :: DrvAddr
-   , d_data :: Bits 11
-   }
-|]
-
-w_c1 :: MSG
-w_c1 = m' drv_write drv_control1 $
-  repToBits $ withBits 0 $ do
-    setField drv_ocmode drv_ocmode_latch_shutdown
-    setField drv_ocadj drv_ocadj_0p730
-
-w_c2 :: MSG
-w_c2 = m' drv_write drv_control2 $
-  repToBits $ withBits 0 $ do
-    setField drv_gain drv_gain_40
-
-r_c1 :: MSG
-r_c1 = m drv_read drv_control1
-
-r_c2 :: MSG
-r_c2 = m drv_read drv_control2
-
-r_s1 :: MSG
-r_s1 = m drv_read drv_status1
-
-r_s2 :: MSG
-r_s2 = m drv_read drv_status2
-
--- for odrive this is C1 $11 = 0x1550
--- and C2 $12 = 0x1808, status1 0x0, status2 0x801
-
-m :: DrvRW -> DrvAddr -> MSG
-m rw addr = fromRep $ withBits 0 $ do
-  setField d_rw rw
-  setField d_addr addr
-
-m' :: DrvRW -> DrvAddr -> Bits 11 -> MSG
-m' rw addr c = fromRep $ withBits 0 $ do
-  setField d_rw rw
-  setField d_addr addr
-  setField d_data c
-
-spi_req :: (GetAlloc eff ~ 'Scope s)
-        => SPIDeviceHandle
-        -> MSG
-        -> Ivory eff (ConstRef ('Stack s) ('Struct "spi_transaction_request"))
-spi_req dev msg = fmap constRef $ local $ istruct
-              [ tx_device .= ival dev -- (SPIDeviceHandle 0)
-              , tx_buf    .= iarray [h msg, l msg]
-              , tx_len    .= ival 2
-              ]
-  where l x = ival $ bitCast $ toRep x
-        h x = ival $ bitCast $ (toRep x) `iShiftR` 8
-
-
-drvTower :: (IvoryZero init, IvoryArea init) =>
-            BackpressureTransmit
-              ('Struct "spi_transaction_request")
-              ('Struct "spi_transaction_result")
-            -> ChanOutput init
-            -> ChanInput ('Stored Uint8)
-            -> SPIDeviceHandle
-            -> Tower e ()
-drvTower (BackpressureTransmit req_c res_c) init_chan ostream dev = do
-  periodic <- period (Milliseconds 500)
-
-  monitor "drv8301mon" $ do
-    ready <- stateInit "ready" (ival false)
-    retries <- stateInit "retries" (ival (0 :: Uint8))
-    rounds <- stateInit "rounds" (ival (0 :: Uint8))
-    handler systemInit "init" $ do
-      callback $ const $ do
-        -- enable en_gate pin connected to both DRVs
-        pinOut drv8301_en_gate
-        pinHigh drv8301_en_gate
-
-    coroutineHandler init_chan res_c "drv8301" $ do
-      req_e <- emitter req_c 1
-      o <- emitter ostream 64
-      return $ CoroutineBody $ \ yield -> do
-        comment "drv coro start"
-        let (SPIDeviceHandle x) = dev
-        puts o "start"
-        putc o (48 + x)
-        -- rpc is nice, you emit request and yield
-        -- while your request is processed
-        --let rpc req = req >>= emit req_e >> yield
-        -- but IT RESPONDS IN THE NEXT CYCLE!!!
-        -- so we emit twice and return the second reply
-        --let rpc req = req >>= emit req_e >> yield >> req >>= emit req_e >> yield
-        -- which is ~equal to
-        let rpc req = do
-              newreq <- req
-              emit req_e newreq
-              _ <- yield -- previous command response
-              emit req_e newreq
-              yield -- our response
-
-        let putResp e r = do
-              arrayMap $ \ix -> do
-                when (fromIx ix <? 2) $ do
-                  val <- deref ((r ~> rx_buf) ! ix)
-                  --putc o (48 + (castWith 0 $ fromIx $ ix))
-                  comment "drv put"
-                  putc e val
-
-        comment "Initialization routine"
-        comment "Loop until register changes responding to our write or we reach retries"
-        forever $ do
-          _ <- rpc (spi_req dev w_c1)
-          r <- rpc (spi_req dev r_c1)
-
-          rval <- deref ((r ~> rx_buf) ! 0)
-          --puts o "r"
-          --putc o rval
-
-          -- FIXME: should compare to w_c1 data, I know it's 0x1550 so I can cheat for now
-          when (rval ==? 0x15) breakOut
-
-          rs <- deref retries
-          store retries (rs + 1)
-
-          -- XXX: handle init timeout? or emit on some ready channel
-          -- when initialized
-          when (rs >? 100) retVoid
-
-        store ready true
-
-        comment "drv coro forever"
-        forever $ do
-          comment "wait for response triggered by periodic request"
-          --- XXX: handle status register errors, emit them on separate chan
-          r0 <- yield
-          return ()
-          comment "debug helpers"
-          r <- deref rounds
-          ifte_ (r ==? 200) (puts o "-" >> store rounds (r+1)) (store rounds (r+1))
-          putc o (48 + x)
-          puts o "i"
-          putResp o r0
-
-    handler periodic "periodic" $ do
-      req_e <- emitter req_c 1
-      callback $ \_ -> do
-        isReady <- deref ready
-        -- FIXME: read both status registers
-        when isReady $ (spi_req dev r_s1) >>= emit req_e
 
 app :: (e -> ClockConfig)
     -> (e -> TestSPI)
@@ -213,12 +62,12 @@ app tocc totestspi touart toleds = do
         emit e t
 
   (drvTask0, drvReq0) <- task "drv8301_m0"
-  drvTower drvReq0 (snd initdone_sready) ostream (SPIDeviceHandle 0)
+  drvTower drvReq0 (snd initdone_sready) (SPIDeviceHandle 0)
 
   (drvTask1, drvReq1) <- task "drv8301_m1"
-  drvTower drvReq1 (snd initdone_sready) ostream (SPIDeviceHandle 1)
+  drvTower drvReq1 (snd initdone_sready) (SPIDeviceHandle 1)
 
-  schedule (spiName $ testSPIPeriph spi)
+  schedule ("drv")
     [drvTask0, drvTask1] sready sreq
 
   periodic <- period (Milliseconds 500)
