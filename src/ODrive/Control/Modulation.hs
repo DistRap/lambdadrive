4dc67e2ebc90c16eb24d0cd5b35be4de8ef7ffa6
Richard Marko <srk@48.io>
2017-05-04 22:51:52 +0200

    add ODrive.Control.Modulation

---
+++ src/ODrive/Control/Modulation.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+module ODrive.Control.Modulation where
+
+import Ivory.Language
+import Ivory.Stdlib
+
+import ODrive.Platforms (tim_period_clocks)
+import ODrive.Control.SVM (svm)
+import ODrive.Ivory.Types.Svm
+
+current_modulation :: (GetAlloc eff ~ 'Scope s)
+              => IFloat
+              -> IFloat
+              -> Ref s1 ('Array 3 ('Stored Uint16))
+              -> Ivory eff ()
+current_modulation alpha beta pwmout = do
+        svmout <- svm alpha beta
+        pA <- deref (svmout ~> svm_a)
+        pB <- deref (svmout ~> svm_b)
+        pC <- deref (svmout ~> svm_c)
+        store (pwmout ! 0) (castDefault $ pA * tim_period_clocks_float)
+        store (pwmout ! 1) (castDefault $ pB * tim_period_clocks_float)
+        store (pwmout ! 2) (castDefault $ pC * tim_period_clocks_float)
+  where
+    tim_period_clocks_float :: IFloat
+    tim_period_clocks_float = safeCast tim_period_clocks
+
+voltage_modulation :: (GetAlloc eff ~ 'Scope s)
+              => IFloat
+              -> IFloat
+              -> IFloat
+              -> Ref s1 ('Array 3 ('Stored Uint16))
+              -> Ivory eff ()
+voltage_modulation vbus alpha beta pwmout = do
+    let vfactor = 1.0 / ((2.0 / 3.0) * vbus)
+    current_modulation (vfactor * alpha) (vfactor * beta) pwmout
